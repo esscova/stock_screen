cd3151f27aa23b4a8911919f471b880a50a9b7bd
wellington moreira <wsantos08@hotmail.com>
2024-12-12 13:56:34 -0300

    feat: adicionado função para obtenção de dados de um ativo.

--- main.py
+++ main.py
@@ -13,8 +13,31 @@ from loguru import logger
 logger.add("logs/debug.log", format="{time} {level} {message}", level="DEBUG", rotation="1 MB", compression="zip")
 
 ##########################################################################################
-## PARTE 1: FUNÇÕES PARA OBTER DADOS ##
+## PARTE 1: FUNÇÕES PARA OBTER E TRATAR DADOS ##
 ##########################################################################################
+def obter_dados(ticker, intervalo):
+    try: # Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
+        ativo = yf.Ticker(ticker)
+        if not ativo:
+            logger.error('Falha em obter dados')
+            raise 
+
+        cotacoes = ativo.history(period='max', interval = intervalo)
+        logger.info('Dados de cotações obtidos com sucesso.')
+
+        info = {
+            'nome':ativo.info['longName'],
+            'setor': ativo.info['sector'],
+            'segmento': ativo.info['industry'],
+            'dividend yield': ativo.info['dividendYield'],
+            'ultimo dividendo': ativo.info['lastDividendValue']
+        }
+        logger.info('Dados do ativo obtidos com sucesso.')
+        return cotacoes, info
+    
+    except:
+        raise
+
 
 ##########################################################################################
 ## PARTE 2: PAINEL DO STREAMLIT ##
@@ -32,12 +55,27 @@ st.title("Stocks Dashboard")
 with st.sidebar:
     st.header("Filtros")
     ticker = st.text_input("Ticker", "AAPL")
-    periodo = st.selectbox('Período', ['1m','5m', '15m', '30m', '1h', '1d', '5d', '1wk', '1mo', '3mo', 'Max'])
+    intervalo = st.selectbox('Período', ['1m', '2m', '5m', '15m', '30m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo'])
     grafico = st.selectbox('Tipo de Gráfico', ['Line', 'Candlestick'])
     indicadores = st.multiselect('Médias móveis', ['SMA 20', 'EMA 20'])
+    update = st.button('UPDATE')
 
-    if st.button('UPDATE'):
-        st.write('Atualizando...')
-    
     st.subheader("Sobre")
     st.info("Desenvolvido por [escova](https://github.com/esscova) com Streamlit.")
+
+
+if update:
+    cotacoes, info = obter_dados(ticker=ticker, intervalo=intervalo) 
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.write('Cotações')
+        st.dataframe(cotacoes)
+    
+    with col2:
+        st.write('Informações')
+        st.text(info['nome'])
+
+
+    
